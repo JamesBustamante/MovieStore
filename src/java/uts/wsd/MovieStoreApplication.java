ec146a39234beacd509fad73fb5491782987fb77
james <james@DESKTOP-R8MICJR>
2018-09-06 20:18:39 +1000

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

---
+++ src/java/uts/wsd/MovieStoreApplication.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.wsd;
+
+import java.io.*;
+import javax.xml.bind.*;
+/**
+ *
+ * @author james
+ */
+public class MovieStoreApplication {
+        private String filePath;
+        private Users users;
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) throws Exception {
+        // Create the unmarshaller
+        JAXBContext jc = JAXBContext.newInstance(Users.class);
+        Unmarshaller u = jc.createUnmarshaller();
+        this.filePath = filePath;
+        FileInputStream fin = new FileInputStream(filePath);
+        users = (Users)u.unmarshal(fin); // This loads the "shop" object
+        fin.close();
+    }
+    
+        public void updateXML(Users users, String filePath) throws Exception {
+        this.users = users;
+        this.filePath = filePath;
+        JAXBContext jc = JAXBContext.newInstance(Users.class);
+        Marshaller m = jc.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        FileOutputStream fout = new FileOutputStream(filePath);
+        m.marshal(users, fout);
+        fout.close();
+    }
+        
+        public void saveUsers() throws JAXBException, IOException {
+        JAXBContext jc = JAXBContext.newInstance(Users.class);
+        Marshaller m = jc.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        FileOutputStream fout = new FileOutputStream(filePath);
+        m.marshal(users, fout);
+        fout.close();
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+        
+        
+}
---
+++ src/java/uts/wsd/User.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.wsd;
+import javax.xml.bind.annotation.*;
+import java.util.*;
+import java.io.Serializable;
+/**
+ *
+ * @author james
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "user")
+public class User implements Serializable {
+    @XmlElement(name = "id")
+    private String id;
+    @XmlElement(name = "email")
+    private String email;
+    @XmlElement(name = "password")
+    private String password;
+    @XmlElement(name = "fullname")
+    private String fullName;
+    @XmlElement(name = "phone")
+    private String phone;
+    @XmlElement(name = "address")
+    private String address;
+    @XmlElement(name = "gender")
+    private String gender;
+
+    public User(String id, String email, String password, String fullName, String phone, String address, String gender) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.fullName = fullName;
+        this.phone = phone;
+        this.address = address;
+        this.gender = gender;
+    }
+    
+      public User() {}
+      
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+        public boolean matchEmail(String email) {
+        return this.email.equals(email.trim());
+    }
+
+    
+}
